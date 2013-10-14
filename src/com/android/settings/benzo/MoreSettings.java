e0043a3ed281e59270a832e4f781741eeb9ad7ff
maxwen <max.weninger@gmail.com>
2013-10-14 20:56:04 +0200

    [2/2] settings: add CPU info overlay
    this shows
    CPU temp (if available)
    for every core governor + online + freq
    
    BenzoEdit:
    Moved it out of Developer Settings
    
    Change-Id: I28c1e48c2e613b33c301b61cead70678ab6a2e9b

--- src/com/android/settings/benzo/MoreSettings.java
+++ src/com/android/settings/benzo/MoreSettings.java
@@ -15,11 +15,16 @@
 */
 package com.android.settings.benzo;
 
+import android.app.Activity;
+import android.app.ActivityManagerNative;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.preference.ListPreference;
@@ -40,9 +45,11 @@ public class MoreSettings extends SettingsPreferenceFragment implements OnPrefer
 
     private static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
     private static final String PREF_TRANSPARENT_VOLUME_DIALOG = "transparent_volume_dialog";
+    private static final String SHOW_CPU_INFO_KEY = "show_cpu_info";
 
     private ListPreference mMsob;
     private SeekBarPreference mVolumeDialogAlpha;
+    private SwitchPreference mShowCpuInfo;
 
     @Override
     protected int getMetricsCategory() {
@@ -54,6 +61,10 @@ public class MoreSettings extends SettingsPreferenceFragment implements OnPrefer
         super.onCreate(icicle);
         final ContentResolver resolver = getActivity().getContentResolver();
         addPreferencesFromResource(R.xml.more_settings);
+
+        mShowCpuInfo = (SwitchPreference) findPreference(SHOW_CPU_INFO_KEY);
+        mShowCpuInfo.setChecked(Settings.Global.getInt(getActivity().getContentResolver(),
+            Settings.Global.SHOW_CPU, 0) == 1);
 
         mMsob = (ListPreference) findPreference(MEDIA_SCANNER_ON_BOOT);
         mMsob.setValue(String.valueOf(Settings.System.getInt(getActivity().getContentResolver(),
@@ -76,7 +87,12 @@ public class MoreSettings extends SettingsPreferenceFragment implements OnPrefer
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
+        if (preference == mShowCpuInfo) {
+            writeCpuInfoOptions();
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+        return false;
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -94,5 +110,18 @@ public class MoreSettings extends SettingsPreferenceFragment implements OnPrefer
             return true;
         }
         return false;
+    }
+
+    private void writeCpuInfoOptions() {
+        boolean value = mShowCpuInfo.isChecked();
+        Settings.Global.putInt(getActivity().getContentResolver(),
+                Settings.Global.SHOW_CPU, value ? 1 : 0);
+        Intent service = (new Intent())
+                .setClassName("com.android.systemui", "com.android.systemui.CPUInfoService");
+        if (value) {
+            getActivity().startService(service);
+        } else {
+            getActivity().stopService(service);
+        }
     }
 }
