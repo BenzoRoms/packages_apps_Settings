dc914137fa37cd3ecdb8d9a841e3ca9b5b84f084
Roman Birg <roman@cyngn.com>
2014-12-05 15:54:34 +0000

    Utils: add isPackageInstalled boolean
    Change-Id: I7acf7480446c364a54e0b49214bad814c0e876b9

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1176,5 +1176,20 @@ public final class Utils {
             return UserHandle.myUserId();
         }
     }
-}
 
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        if (pkg == null) {
+            return false;
+        }
+        try {
+            PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+            if (!pi.applicationInfo.enabled) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+}
