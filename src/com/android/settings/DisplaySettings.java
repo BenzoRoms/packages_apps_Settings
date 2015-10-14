48d32a08f14466743f568b43389bbbdd82ffe235
mydongistiny <jaysonedson@gmail.com>
2015-10-14 15:42:30 -0700

    Revert "Banish Theme setting to developer options"
    This reverts commit df283601c571e74015c9c79c7d2d5865d1a8c9ee.
    
    Change-Id: I65fe660e4000373dc1e1baf9e10a3652de67d287

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -218,13 +218,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         }
 
         mNightModePreference = (ListPreference) findPreference(KEY_NIGHT_MODE);
-        if (mNightModePreference != null) {
-            final UiModeManager uiManager = (UiModeManager) getSystemService(
-                    Context.UI_MODE_SERVICE);
-            final int currentNightMode = uiManager.getNightMode();
-            mNightModePreference.setValue(String.valueOf(currentNightMode));
-            mNightModePreference.setOnPreferenceChangeListener(this);
-        }
+        final UiModeManager uiManager = (UiModeManager) getSystemService(
+                Context.UI_MODE_SERVICE);
+        final int currentNightMode = uiManager.getNightMode();
+        mNightModePreference.setValue(String.valueOf(currentNightMode));
+        mNightModePreference.setOnPreferenceChangeListener(this);
 
         mWakeWhenPluggedOrUnplugged =
                 (SwitchPreference) findPreference(KEY_WAKE_WHEN_PLUGGED_OR_UNPLUGGED);
