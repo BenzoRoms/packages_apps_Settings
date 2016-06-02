SuperKangerRom/android_packages_apps_Settings-AOSP

Floating Windows settings

src/com/android/settings/SettingsActivity.java
@@ -138,6 +138,7 @@ import com.android.settings.vrtoxin.AppCircleBar;
 import com.android.settings.vrtoxin.AppSideBar;
 import com.android.settings.vrtoxin.HwKeySettings;
 import com.android.settings.vrtoxin.ThemeSettings;
+import com.android.settings.vrtoxin.FloatingWindows;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -376,7 +377,8 @@ public class SettingsActivity extends Activity
             AppCircleBar.class.getName(),
             AppSideBar.class.getName(),
             HwKeySettings.class.getName(),
-            ThemeSettings.class.getName()
+            ThemeSettings.class.getName(),
+            FloatingWindows.class.getName(),
     };
 
 

BenzoRoms/packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -79,6 +79,7 @@ import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
+import com.android.settings.benzo.FloatingWindows;
 import com.android.settings.benzo.MainSettings;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
@@ -362,7 +363,8 @@ public class SettingsActivity extends Activity
             MainSettings.class.getName(),
             com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
             BlacklistSettings.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            FloatingWindows.class.getName(),
     };
 
 