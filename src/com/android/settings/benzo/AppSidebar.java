PAC-ROM/android_packages_apps_Settings

Settings: Increase default width size for Gesture Anywhere and App Sidebar
Change-Id: If20821c922524d77844e33cac84404f71ec45ddb
Signed-off-by: Lokesh Chamane <lokesh.chamane@pac-rom.com>

src/com/android/settings/pac/AppSidebar.java
@@ -76,7 +76,7 @@ public class AppSidebar extends SettingsPreferenceFragment implements
 
         mTriggerWidthPref = (PacSeekBarPreference) findPreference(KEY_TRIGGER_WIDTH);
         mTriggerWidthPref.setValue(Settings.PAC.getInt(getContentResolver(),
-                Settings.PAC.APP_SIDEBAR_TRIGGER_WIDTH, 10));
+                Settings.PAC.APP_SIDEBAR_TRIGGER_WIDTH, 20));
         mTriggerWidthPref.setOnPreferenceChangeListener(this);
 
         mTriggerTopPref = (PacSeekBarPreference) findPreference(KEY_TRIGGER_TOP);

src/com/android/settings/pac/gestureanywhere/GestureAnywhereSettings.java
@@ -70,7 +70,7 @@ public class GestureAnywhereSettings extends SettingsPreferenceFragment implemen
 
         mTriggerWidthPref = (PacSeekBarPreference) findPreference(KEY_TRIGGER_WIDTH);
         mTriggerWidthPref.setValue(Settings.PAC.getInt(getContentResolver(),
-                Settings.PAC.GESTURE_ANYWHERE_TRIGGER_WIDTH, 10));
+                Settings.PAC.GESTURE_ANYWHERE_TRIGGER_WIDTH, 20));
         mTriggerWidthPref.setOnPreferenceChangeListener(this);
 
         mTriggerTopPref = (PacSeekBarPreference) findPreference(KEY_TRIGGER_TOP);

BenzoRoms/packages_apps_Settings

src/com/android/settings/benzo/AppSidebar.java
@@ -71,7 +71,7 @@ public class AppSidebar extends SettingsPreferenceFragment implements
 
         mTriggerWidthPref = (SeekBarPreferenceCHOS) findPreference(KEY_TRIGGER_WIDTH);
         mTriggerWidthPref.setValue(Settings.System.getInt(getContentResolver(),
-                Settings.System.APP_SIDEBAR_TRIGGER_WIDTH, 10));
+                Settings.System.APP_SIDEBAR_TRIGGER_WIDTH, 20));
         mTriggerWidthPref.setOnPreferenceChangeListener(this);
 
         mTriggerTopPref = (SeekBarPreferenceCHOS) findPreference(KEY_TRIGGER_TOP);

src/com/android/settings/benzo/gestureanywhere/GestureAnywhereSettings.java
@@ -77,7 +77,7 @@ public class GestureAnywhereSettings extends SettingsPreferenceFragment implemen
 
         mTriggerWidthPref = (SeekBarPreference) findPreference(KEY_TRIGGER_WIDTH);
         mTriggerWidthPref.setValue(Settings.System.getInt(getContentResolver(),
-                Settings.System.GESTURE_ANYWHERE_TRIGGER_WIDTH, 10));
+                Settings.System.GESTURE_ANYWHERE_TRIGGER_WIDTH, 20));
         mTriggerWidthPref.setOnPreferenceChangeListener(this);
 
         mTriggerTopPref = (SeekBarPreference) findPreference(KEY_TRIGGER_TOP);