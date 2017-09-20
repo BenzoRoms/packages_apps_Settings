50953187353f94c99ba5b25ef49bc149dbe14ce8
mydongistiny <jaysonedson@gmail.com>
2017-09-20 02:31:16 -0700

    Settings: Remove Google Legal and System Update
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -54,8 +54,6 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
 
     private static final String LOG_TAG = "DeviceInfoSettings";
 
-    private static final String KEY_LEGAL_CONTAINER = "legal_container";
-
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.DEVICEINFO;
@@ -158,13 +156,6 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
                 public List<PreferenceController> getPreferenceControllers(Context context) {
                     return buildPreferenceControllers(context, null /*activity */,
                             null /* fragment */, null /* lifecycle */);
-                }
-
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    List<String> keys = super.getNonIndexableKeys(context);
-                    keys.add(KEY_LEGAL_CONTAINER);
-                    return keys;
                 }
             };
 }
