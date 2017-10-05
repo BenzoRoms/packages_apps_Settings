CarbonROM/android_packages_apps_Settings

Settings: Integrate BenzoExtras into Settings
Based on both DU's Nougat and Omni's Oreo implimentations, so
credits to mazda and maxwen for this.

@mydongistiny: fixed up some for dashboard

Change-Id: Id4e5367bab5826bfc8bced5684fc2e599f8140c4
Signed-off-by: Joe Maples <joe@frap129.org>
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

src/com/android/settings/Settings.java
@@ -178,4 +178,5 @@ public class Settings extends SettingsActivity {
     public static class SystemDashboardActivity extends SettingsActivity {}
     public static class SupportDashboardActivity extends SettingsActivity {}
 
+    public static class CarbonFibersActivity extends SettingsActivity { /* empty */ }
 }

BenzoRoms/packages_apps_Settings

src/com/android/settings/Settings.java
@@ -178,4 +178,5 @@ public class Settings extends SettingsActivity {
     public static class SystemDashboardActivity extends SettingsActivity {}
     public static class SupportDashboardActivity extends SettingsActivity {}
 
+    public static class BenzoSettingsActivity extends SettingsActivity { /* empty */ }
 }